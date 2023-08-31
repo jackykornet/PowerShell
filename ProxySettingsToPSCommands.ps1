27670d2e6813ee04a4ce1ea6397eab7daab565d9
jackykornet <143727313+jackykornet@users.noreply.github.com>
2023-08-31 16:59:46 +0200

    Update ProxySettingsToPSCommands.ps1
    added qoutes to output

--- ProxySettingsToPSCommands.ps1
+++ ProxySettingsToPSCommands.ps1
@@ -3,8 +3,8 @@
 $CUProxySettings = Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' | Select-Object ProxyServer, ProxyEnable,ProxyOverride
 
 # output proxy commands to host console
-Write-Host 'Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings -name ProxyServer -Value' $($CUProxySettings.ProxyServer)
-Write-Host 'Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings -name ProxyEnable -Value' $($CUProxySettings.ProxyEnable)
-Write-Host 'Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings -name ProxyOverride -Value' $($CUProxySettings.ProxyOverride)
+Write-Host "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyServer -Value '$($CUProxySettings.ProxyServer)'" 
+Write-Host "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyEnable -Value '$($CUProxySettings.ProxyEnable)'"
+Write-Host "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyOverride -Value '$($CUProxySettings.ProxyOverride)'"
 
-# copy output to an ps1 file or paste them in a powershell prompt        
+# copy output to an ps1 file or paste them in a powershell prompt
