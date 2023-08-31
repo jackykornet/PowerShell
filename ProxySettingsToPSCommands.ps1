1637ae77836ebedfaeb2b2825c0a6f89b80649f3
jackykornet <143727313+jackykornet@users.noreply.github.com>
2023-08-31 16:43:20 +0200

    Update ProxySettingsToPSCommands.ps1
    add some comments

--- ProxySettingsToPSCommands.ps1
+++ ProxySettingsToPSCommands.ps1
@@ -1,8 +1,10 @@
 # get current user proxy settings
+# you can get all proxy settings with Get-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings if you need to add more
 $CUProxySettings = Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' | Select-Object ProxyServer, ProxyEnable,ProxyOverride
 
 # output proxy commands to host console
 Write-Host 'Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings -name ProxyServer -Value' $($CUProxySettings.ProxyServer)
 Write-Host 'Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings -name ProxyEnable -Value' $($CUProxySettings.ProxyEnable)
 Write-Host 'Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings -name ProxyOverride -Value' $($CUProxySettings.ProxyOverride)
-        
+
+# copy output to an ps1 file or paste them in a powershell prompt        
